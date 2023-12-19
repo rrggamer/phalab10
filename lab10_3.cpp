9873d1a60c544cd9206b2c339d035c71fee6fb3f
Your Name <you@example.com>
2023-12-19 15:43:38 +0700

    lab10_3.cpp completed

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,39 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    string myscore;
+    
+    ifstream source;
+    source.open("score.txt");
+    ifstream MyReadFile("score.txt");
+    
+    int count = 0;
+    float sum = 0;
+    float num;
+    float sumsum;
+    float mean;
+
+    while(getline(MyReadFile,myscore))
+    {
+        count++;
+        num = atof(myscore.c_str());
+        sum += atof(myscore.c_str());
+        sumsum += pow(num,2);
+    }
+    
+    mean = sum/count;
+    float stddev = sqrt((sumsum / count) - pow(mean, 2));
+    
+    
+    cout << "Number of data = " << count << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << mean << endl;
+    cout << "Standard deviation = " << stddev;
+}
